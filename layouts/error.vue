4562b7fc8432f3634a013d45b7a195ff540348af
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-10-11 22:38:06 +0700

    -

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,26 @@
 <template>
     <div class="container">
-        <h1 v-if="error.statusCode === 404">Page not found</h1>
-        <h1 v-else>An error occurred</h1>
-        <NuxtLink to="/">Home page</NuxtLink>
+        <template v-if="error.statusCode === 404">
+            <a-result status="404" title="404" sub-title="Xin lỗi, trang bạn truy cập không tồn tại.">
+                <template #extra>
+                    <NuxtLink to="/">Trang chủ</NuxtLink>
+                </template>
+            </a-result>
+        </template>
+        <template v-else>
+            <a-result status="500" title="500" sub-title="Xin lỗi, máy chủ sai.">
+                <template #extra>
+                    <a-button type="primary"> Back Home </a-button>
+                </template>
+            </a-result>
+        </template>
     </div>
 </template>
 
 <script>
 export default {
-    props: ["error"]
+    props: ["error"],
+    layout: "error"
 };
 </script>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
-    css: ["@/assets/packages/ant.less", "@/assets/sass/app.scss"],
+    css: ["@/assets/sass/app.scss", "@/assets/packages/ant.less"],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: ["@/plugins/antd-ui"],
