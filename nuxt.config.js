15395e5661cea9f4acfc881aab1c0e60485b43cb
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-10-17 01:00:52 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
             { name: "format-detection", content: "telephone=no" }
         ],
         link: [
-            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            { rel: "icon", type: "image/x-icon", href: "/icon.png" },
             { rel: "preconnect", href: "https://fonts.googleapis.com" },
             { rel: "preconnect", href: "https://fonts.gstatic.com" },
             { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" }
